dba5ae6fc94c2e01c48337e9ddb3fba28e820cda
Alan Ng <alan.ng@blake.com.au>
2019-04-19 13:46:48 +1000

    wip

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default class App extends React.Component {
       uri: './assets/images/robot-dev.png'
     };
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "start" }}>
-        <Text style={{fontSize: 32, fontWeight: bold}}>Cat App</Text>
+      <View style={{ backgroundColor: "#f4ffff", flex: 1, justifyContent: "center", alignItems: "start" }}>
+        <Text style={{fontSize: 32, fontWeight: "bold"}}>Cat App</Text>
       </View>
     );
   }
